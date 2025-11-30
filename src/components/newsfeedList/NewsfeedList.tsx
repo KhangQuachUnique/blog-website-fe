eadf74f545a3c1390a72fc1f2b6d9e65ca4e1ac1
trinhnguyen1101 <trinhnguyen11012005@gmail.com>
2025-11-30 17:49:46 +0700

    style(newsfeedlist): theo ý e khang

--- src/components/newsfeedList/NewsfeedList.tsx
+++ src/components/newsfeedList/NewsfeedList.tsx
@@ -2,26 +2,44 @@
 import Card from '../card/Card';
 import type  { PostItem } from '../../types/post';
 import { Loader2 } from 'lucide-react';
+import Masonry from 'react-masonry-css';
 import '../../styles/newsfeed/NewsfeedList.css';
 
 interface Props {
   posts: PostItem[];
   isFetchingNextPage?: boolean;
   hasNextPage?: boolean;
-  loadMoreRef?: (node: HTMLDivElement) => void;
+  loadMoreRef?: (node: HTMLDivElement | null) => void;
 }
 
-// NewsfeedList.tsx
+// NewsfeedList.tsx using react-masonry-css
 const NewsfeedList = ({ posts, loadMoreRef }: Props) => {
-  return (
-    <div className="newsfeed-list justify-start" ref={loadMoreRef}>
-  {posts.map((post) => (
-    <Card key={post.id} post={post} />
-  ))}
-</div>
+  const breakpointCols = {
+    default: 4,
+    1400: 4,
+    992: 3,
+    768: 2,
+    0: 1,
+  };
 
+  return (
+    <div className="newsfeed-list-wrapper">
+      <Masonry
+        breakpointCols={breakpointCols}
+        className="newsfeed-masonry-grid"
+        columnClassName="newsfeed-masonry-grid_column"
+      >
+        {posts.map((post) => (
+          <div key={post.id} className="newsfeed-masonry-item">
+            <Card post={post} />
+          </div>
+        ))}
+      </Masonry>
+
+      {/* sentinel for infinite scroll */}
+      <div ref={loadMoreRef as any} />
+    </div>
   );
 };
-
 
 export default NewsfeedList;
